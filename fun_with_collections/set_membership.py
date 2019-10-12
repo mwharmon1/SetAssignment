c5679f12cdbe8f885b7258050aa5cc54e117b95b
Michael Harmon <mwharmon1@dmacc.edu>
2019-10-11 21:19:43 -0500

    Added functionality for in_set(). Tests are passing.

--- fun_with_collections/set_membership.py
+++ fun_with_collections/set_membership.py
@@ -8,13 +8,17 @@ is in a set.
 
 def in_set(my_set, my_value):
     """
-    check for element in set, return true if found or false if not found
+    check for element in set
     :param my_set: set to search
     :param my_value: element to find in set
     :return: true for found, false for not found
     """
-    pass
+    if my_value in my_set:
+        return True
+    return False
 
 
 if __name__ == '__main__':
-    in_set()
+    this_value = 2
+    this_set = {1, 2, 3, 4}
+    print(in_set(this_set, this_value))
